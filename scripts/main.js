eb4ba87022c742e27277f4e763ea838dba8064fc
kyuyeop <44936443+kyuyeop@users.noreply.github.com>
2022-11-04 20:16:40 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -45,6 +45,6 @@ if(!Vars.headless){
         tc.top().left();
         addTable(tc);
         Vars.ui.hudGroup.addChild(tc);
-        if(Vars.mobile) tc.moveBy(0, Scl.scl(1));
+        if(Vars.mobile) tc.moveBy(0, Scl.scl(-100));
     });
 }
