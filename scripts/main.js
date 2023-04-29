35a7cfbf2bdffc47c19152d69c661a781f799634
kyuyeop <44936443+kyuyeop@users.noreply.github.com>
2023-04-29 19:43:57 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,26 @@
-let cols = [Pal.lancerLaser, Pal.accent, Color.valueOf("cc6eaf")];
-
 let v = 0;
 
 function addTable(table){
     table.table(Tex.pane, t => {
         let bm = t.button("<", () => {
-            if(v > -9)
+            if(v > -5)
                 v--;
-            else
-                v = 13;
             Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * Math.pow(2, v), 3 * Math.pow(2, v)));
-            l.color(Tmp.c1.lerp(cols, (v + 9) / 22));
         }).width(44).get();
         let l = t.label(() => {
             if(v >= 0)
                 return "x" + Math.pow(2, v);
             else
                 return "x1/" + Math.pow(2, Math.abs(v));
-        }).growX().width(8.5 * 8).color(Pal.accent);
+        }).growX().width(8.5 * 7).color(Pal.accent);
         let bp = t.button(">", () => {
-            if(v < 13)
+            if(v < 5)
                 v++;
-            else
-                v = -9;
             Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * Math.pow(2, v), 3 * Math.pow(2, v)));
-            l.color(Tmp.c1.lerp(cols, (v + 9) / 22));
         }).padLeft(6).width(44).get();
         //let reset = t.button("1", () => {
         //    v = 0;
         //    Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * Math.pow(2, v), 3 * Math.pow(2, v)));
-        //    l.color(Tmp.c1.lerp(cols, (v + 9) / 22));
         //}).padLeft(1).width(44).get();
     });
     table.visibility = () => {
