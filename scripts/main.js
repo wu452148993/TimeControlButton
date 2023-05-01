2577addabd515597d53049f835e9d59588f05b68
kyuyeop <44936443+kyuyeop@users.noreply.github.com>
2023-05-01 23:56:28 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -36,7 +36,7 @@ if(!Vars.headless){
     var tc = new Table();
     Events.on(ClientLoadEvent, () => {
         addTable(tc);
-        var marker = Vars.ui.hudGroup.find("overlaymarker").find("waves/editor").find("waves").find("statustable").find("status");
+        var marker = Vars.ui.hudGroup.find("overlaymarker").find("waves/editor").find("waves").find("statustable").find("status").parent;
         marker.row();
         //marker.button("Button", () => {});
         marker.add(tc);
